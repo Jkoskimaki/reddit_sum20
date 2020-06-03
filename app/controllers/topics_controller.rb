0f91aa72fd2a436d2d933fdac72d226dc09c6821
jacob koskimaki <jkoskimaki@gmail.com>
2020-06-02 20:57:57 -0400

    topic controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,64 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :destroy, :update]
+  before_action :set_sub
+  #want to be set for every single action ... all routes for topic have a sub_id, e.g.
+
+  # Topic --> sub.topics
+
   def index
+    @topics = @sub.topics
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def new
+    @topic = @sub.topics.new
+    render partial: "form"
   end
 
-  def edit--skip-routes
+  def create
+    @topic = @sub.topics.new(topic_params)
+    if @topic.save
+      redirect_to [@sub, @topic]
+    else
+      render :new
+    end
   end
+
+  def edit
+    render partial: "form"
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update(topic_params)
+      redirect_to [@sub, @topic]
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to sub_topics_path(@sub)
+  end
+
+
+private
+
+  def topic_params
+    params.require(:topic).permit(:title, :body)
+  end
+
+  def set_topic
+    @topic = Topic.find(params[:id])
+  end
+
+  def set_topic
+    @sub = Sub.find(params[:sub_id])
+  end
+
 end
+
